7ee9f472b39a3cc19a876848cecd9935941c1909
Matt Christiansen <matt@nikore.net>
2013-08-31 20:50:11 -0700

    Upgraded to kafka 0.8

--- kafka-consumer/src/main/scala/net/opentsdb/kafka/consumer/TsdbConsumer.scala
+++ kafka-consumer/src/main/scala/net/opentsdb/kafka/consumer/TsdbConsumer.scala
@@ -5,37 +5,37 @@ import com.google.common.util.concurrent.AbstractIdleService
 import java.util.Properties
 import com.google.inject.name.Named
 import org.slf4j.LoggerFactory
-import kafka.consumer.{ConsumerIterator, ConsumerConfig, Consumer}
+import kafka.consumer.{KafkaStream, ConsumerConfig, Consumer}
 import net.opentsdb.client.netty.TsdbClient
-import scala.collection.immutable.HashMap
-import kafka.message.Message
-import java.nio.ByteBuffer
+import java.util.concurrent.Executors
 
-class TsdbConsumer @Inject() (props: Properties, client: TsdbClient ,@Named("tsdb.kafka.topic") topicName: String) extends AbstractIdleService{
+class TsdbConsumer @Inject() (props: Properties, client: TsdbClient, @Named("tsdb.kafka.topic") topicName: String, @Named("tsdb.kafka.topic.partitions") partitions: Int) extends AbstractIdleService{
   private final val logger = LoggerFactory.getLogger(classOf[TsdbConsumer])
   private final val consumerConnector = Consumer.create(new ConsumerConfig(props))
+  private final val executorPool = Executors.newFixedThreadPool(partitions)
 
   def startUp() {
     logger.info("Starting up TSDB Kafka Consumer")
-    val topicCountMap = HashMap(topicName -> 1)
+    val topicMessageStreams = consumerConnector.createMessageStreams(Predef.Map(topicName -> partitions))
+    val topicStreams: List[KafkaStream[Array[Byte], Array[Byte]]] = topicMessageStreams.get(topicName).get
 
-    val consumerMap = consumerConnector.createMessageStreams(topicCountMap)
-    val stream = consumerMap.get(topicName).get(0)
-    val it: ConsumerIterator[Message] = stream.iterator()
-
-    while(it.hasNext()) {
-      val message = it.next().message
-      val buffer: ByteBuffer = message.payload
-      val bytes = new Array[Byte](buffer.remaining())
-      buffer.get(bytes)
-      val messageString = new String(bytes)
-      logger.debug("Message is: {}", messageString)
-      client.send(messageString)
+    for(stream <- topicStreams) {
+      executorPool.execute(new Runnable() {
+        override def run() {
+          val it = stream.iterator()
+          while(it.hasNext) {
+            val messageString = new String(it.next().message)
+            logger.debug("Message is: {}", messageString)
+            client.send(messageString)
+          }
+        }
+      })
     }
   }
 
   def shutDown() {
     logger.info("Shutting down TSDB Kafka Consumer")
+    executorPool.shutdown()
     consumerConnector.shutdown()
   }
 }
--- opentsdb-proxy/src/main/scala/net/opentsdb/proxy/clients/KafkaProducer.scala
+++ opentsdb-proxy/src/main/scala/net/opentsdb/proxy/clients/KafkaProducer.scala
@@ -3,7 +3,7 @@ package net.opentsdb.proxy.clients
 import com.google.inject.Inject
 import java.util.Properties
 import org.slf4j.LoggerFactory
-import kafka.producer.{ProducerData, ProducerConfig, Producer}
+import kafka.producer.{KeyedMessage, ProducerConfig, Producer}
 import com.google.inject.name.Named
 
 class KafkaProducer @Inject() (props: Properties, @Named("tsdb.kafka.topic") topicId: String) extends Client{
@@ -11,10 +11,8 @@ class KafkaProducer @Inject() (props: Properties, @Named("tsdb.kafka.topic") top
   private final val producer: Producer[String, String] = new Producer(new ProducerConfig(props))
 
   def sendMessage(message: String) {
-    val data: ProducerData[String, String] = new ProducerData(topicId, message)
-
     logger.debug("Sending: {}", message)
 
-    producer.send(data)
+    producer.send(new KeyedMessage[String, String](topicId, message))
   }
 }
